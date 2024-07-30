883234aed36186b9090b4062dabc54a1f548c6c6
samuellawrentz <samuellawrentz@gmail.com>
2024-07-30 15:24:16 +0530

    Added snipe fix

--- after/plugin/snipe.lua
+++ after/plugin/snipe.lua
@@ -1,3 +1,3 @@
     local snipe = require("snipe")
     snipe.setup()
-    vim.keymap.set("n", "<leader>fj", snipe.create_buffer_menu_toggler())
+    vim.keymap.set("n", "<leader>fj", snipe.create_buffer_menu_toggler({ max_path_width = 3}))
